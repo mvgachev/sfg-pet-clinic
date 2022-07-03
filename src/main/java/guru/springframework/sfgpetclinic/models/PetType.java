c86e929ab6652047db3757722e905a274a6cdd44
mg5u19 <--your>
2022-07-03 12:49:44 +0300

    Adding POJOs. Closes #2.

---
+++ src/main/java/guru/springframework/sfgpetclinic/models/PetType.java
@@ -0,0 +1,16 @@
+package guru.springframework.sfgpetclinic.models;
+
+import java.time.LocalDate;
+
+public class PetType {
+
+    String name;
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+}
